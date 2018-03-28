d51bcebf6e0c6efc455a64def8dabb5e424cc984
Tom Dinitz <tdinitz@gmail.com>
2018-03-28 12:54:50 -0400

    Worked on RNN for stocks

--- RNN-Example-Stocks/Stock_Prediction.py
+++ RNN-Example-Stocks/Stock_Prediction.py
@@ -10,36 +10,57 @@ import time #helper libraries
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 from numpy import newaxis
+from pandas import DataFrame
+from pandas import Series
+import argparse
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-s", "--stock", required=True,
+	help="Ticker symbol for stock")
+args = vars(ap.parse_args())
 
-#prices_dataset =  pd.read_csv('../input/prices.csv', header=0)
+###### Define functions to be called below #####
 
-#yahoo = prices_dataset[prices_dataset['symbol']=='YHOO']
-#yahoo_stock_prices = yahoo.close.values.astype('float32')
-#yahoo_stock_prices = yahoo_stock_prices.reshape(1762, 1)
-#yahoo_stock_prices.shape
+# Take stock ticker, and ouput the data for that stock
+def retrieve_data(stock):
+	text=open("Stocks/"+stock+".us.txt").read()
+	dataset=text.splitlines()[1:] #remove header
+	dataset=np.array([[float(num) for num in line.split(',')[1:-1]] for line in dataset])
+	return dataset
 
-#scaler = MinMaxScaler(feature_range=(0, 1))
-#yahoo_stock_prices = scaler.fit_transform(yahoo_stock_prices)
+#This is hard-coded in for now
+close_index=3
 
-#train_size = int(len(yahoo_stock_prices) * 0.80)
-#test_size = len(yahoo_stock_prices) - train_size
-#train, test = yahoo_stock_prices[0:train_size,:], yahoo_stock_prices[train_size:len(yahoo_stock_prices),:]
-#print(len(train), len(test))
+#Create a differenced series
+def difference(dataset,interval=1):
+	return dataset[interval:]-dataset[0:-interval]
 
-# convert an array of values into a dataset matrix
-def create_dataset(dataset, look_back=1):
+# convert data to be input and output for neural net
+# i.e. X=t and Y=t+1
+def create_dataset(dataset, look_back=1, close_index=close_index):
 	dataX, dataY = [], []
-	for i in range(len(dataset)-look_back-1):
-		a = dataset[i:(i+look_back), 0]
-		dataX.append(a)
-		dataY.append(dataset[i + look_back, 0])
+	for i in range(len(dataset)-look_back):
+		input_data = dataset[i:(i+look_back)]
+		input_data = [data[close_index] for data in input_data]
+		dataX.append(input_data)
+		dataY.append(dataset[i + look_back, close_index])
 	return np.array(dataX), np.array(dataY)
 
-# reshape into X=t and Y=t+1
+
+# Retrieve stock data and turn it into list of differences
+stock=args["stock"]
+data=retrieve_data(stock)
+data=difference(data)
+print(len(data))
+
+# Split data into train and test sets
+train_set=data
+test_set=data
+
+# Extract relevant data from train and test sets
 look_back = 1
-trainX, trainY = create_dataset(train, look_back)
-testX, testY = create_dataset(test, look_back)
+trainX, trainY = create_dataset(train_set, look_back)
+testX, testY = create_dataset(test_set, look_back)
 
 trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
 testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
@@ -67,11 +88,13 @@ model.compile(loss='mse', optimizer='rmsprop')
 print ('compilation time : ', time.time() - start)
 
 #Train Model
-model.fit(
-    trainX,
-    trainY,
-    batch_size=128,
-    nb_epoch=10,
-    validation_split=0.05)
+model.fit(trainX,trainY,batch_size=128,nb_epoch=10,validation_split=0.05)
+
+##### THINGS TO ADD
+# Scale values (optional)
+# Split train and test sets
+# more nuanced training
+# testing
+
 
 
